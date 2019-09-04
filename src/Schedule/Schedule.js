b48bb0d94b59e0fe4b44db567a9dc5717e1ac095
Vitalii Retel <vitaly.retel@in2ition.com.ua>
2019-09-04 21:44:10 +0300

    Add schedule for Thursday
    - add Jenkinsfile

--- src/Schedule/Schedule.js
+++ src/Schedule/Schedule.js
@@ -1,60 +1,58 @@
-import React from 'react';
-import './Schedule.css';
+import React from "react";
+import "./Schedule.css";
 
 function Schedule() {
   return (
     <div className="Schedule">
-      <div className="Schedule-day">
+      <div className="Schedule-days">
+        <div className="Schedule-day">
           <p className="Schedule-day-title">Понеділок</p>
           <ul>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
           </ul>
-      </div>
-      <div className="Schedule-day">
+        </div>
+        <div className="Schedule-day">
           <p className="Schedule-day-title">Вівторок</p>
           <ul>
-              <li>Українська мова</li>
-              <li>Я пізнаю світ</li>
-              <li>Математика</li>
-              <li>Фізична культура</li>
+            <li>Українська мова</li>
+            <li>Я пізнаю світ</li>
+            <li>Математика</li>
+            <li>Фізична культура</li>
           </ul>
-      </div>
-      <div className="Schedule-day">
+        </div>
+        <div className="Schedule-day">
           <p className="Schedule-day-title">Середа</p>
           <ul>
-              <li>Українська мова</li>
-              <li>Читання</li>
-              <li>Англійська мова</li>
-              <li>Математика</li>
+            <li>Українська мова</li>
+            <li>Читання</li>
+            <li>Англійська мова</li>
+            <li>Математика</li>
           </ul>
-          <div className="Schedule-day-notes">
-            <p>Неодхідні два зошити:</p>
-            <ul>
-                <li>Українська мова, математика, я пізнаю світ</li>
-                <li>Як добре самому читати</li>
-            </ul>
-          </div>
-      </div>
-      <div className="Schedule-day">
+        </div>
+        <div className="Schedule-day">
           <p className="Schedule-day-title">Четвер</p>
           <ul>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
+            <li>Я пізнаю світ</li>
+            <li>Математика</li>
+            <li>Англійська мова</li>
+            <li>Мистецтво</li>
           </ul>
-      </div>
-      <div className="Schedule-day">
+        </div>
+        <div className="Schedule-day">
           <p className="Schedule-day-title">П'ятниця</p>
           <ul>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
           </ul>
+        </div>
+      </div>
+      <div className="Schedule-day-notes">
+        <p>Всі три зошити носіть разом</p>
       </div>
     </div>
   );
