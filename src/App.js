0530c063a4ceead869d8138a564c0e9372f51677
Vitalii Retel <vitaly.retel@in2ition.com.ua>
2019-09-04 11:04:14 +0300

    Basic page with the schedule

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Menu from './Menu/Menu';
+import Schedule from './Schedule/Schedule';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Старокостянтинівська школа №9, 1-Б клас
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="App-blocks">
+        <div className="App-menu">
+          <Menu />
+        </div>
+        <div className="App-content">
+          <Schedule />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Menu/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Menu.css';
+
+function Menu() {
+  return (
+    <div className="Menu">
+      <a href="#">Розклад</a>
+      <a href="https://drive.google.com/drive/folders/1IvO6w0ZGvKd5rP8ZTb7N08jHXPMEcv90?usp=sharing" target="_blank">Фото (гугл драйв)</a>
+    </div>
+  );
+}
+
+export default Menu;
