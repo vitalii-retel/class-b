3c756fa1aee111aeebcf1aed3b4fb0d97b8287a9
Vitalii Retel <vitaly.retel@in2ition.com.ua>
2019-10-14 14:26:18 +0300

    Update bells schedule

--- src/Bells/Bells.js
+++ src/Bells/Bells.js
@@ -12,37 +12,42 @@ class Bells extends React.Component {
         </div>
         <div className="Bells-row">
           <div>1</div>
-          <div>8.30 - 9.05</div>
+          <div>8.30 - 9.15</div>
           <div>10 хв.</div>
         </div>
         <div className="Bells-row">
           <div>2</div>
-          <div>9.15 - 9.50</div>
+          <div>9.25 - 10.10</div>
           <div>20 хв.</div>
         </div>
         <div className="Bells-row">
           <div>3</div>
-          <div>10.10 - 10.45</div>
+          <div>10.30 - 11.15</div>
           <div>10 хв.</div>
         </div>
         <div className="Bells-row">
           <div>4</div>
-          <div>10.55 - 11.30</div>
-          <div>30 хв.</div>
+          <div>11.25 - 12.10</div>
+          <div>20 хв.</div>
         </div>
         <div className="Bells-row">
           <div>5</div>
-          <div>12.00 - 12.35</div>
+          <div>12.30 - 13.15</div>
           <div>10 хв.</div>
         </div>
         <div className="Bells-row">
           <div>6</div>
-          <div>12.45 - 13.20</div>
+          <div>13.25 - 14.10</div>
           <div>10 хв.</div>
         </div>
         <div className="Bells-row">
           <div>7</div>
-          <div>13.30 - 14.05</div>
+          <div>14.20 - 15.05</div>
+          <div>10 хв.</div>
+        </div>
+        <div className="Bells-row">
+          <div>8</div>
+          <div>15.15 - 16.00</div>
           <div></div>
         </div>
       </div>
